54198f71d73486e1a49ce838fc5b6128822114fa
Zsanta Zhao <598194494@qq.com>
2019-11-07 00:17:27 +0800

    add .views index and static files

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Post
+import hashlib, string, random, time
 # Create your views here.
+
+def index(request):
+    Post_list = Post.objects.all().order_by('published_date')[:5]
+    return render(request, 'blog/HTML/index.html', {'articles': Post_list})
+
