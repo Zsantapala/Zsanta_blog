d37787a55043771978abac17db97a4e6033d6b9d
Zsanta Zhao <598194494@qq.com>
2019-11-08 20:18:23 +0800

    add forms

---
+++ blog/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from captcha.fields import CaptchaField
+class UserForm(forms.Form):
+    username = forms.CharField(label='用户名', max_length=128, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label='密  码', max_length=256, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    #captcha = CaptchaField(label='验证码')
+
+class ArticleForm(forms.Form):
+    headline = forms.CharField(label='标题', max_length=128, widget=forms.TextInput)
+    text_body = forms.CharField(label='内容', widget=forms.Textarea)
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render, redirect
 from .models import Post
-import hashlib, string, random, time
+from .forms import UserForm, ArticleForm
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 def index(request):
     Post_list = Post.objects.all().order_by('published_date')[:5]
     return render(request, 'blog/HTML/index.html', {'articles': Post_list})
 
+def login_status(request):
+    if request.session.get('is_login', None):
+        return redirect('index')
+    if request.method == 'POST':
+        login_form = UserForm(request.POST)
+        message = '请检查填写内容！'
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                request.session['is_login'] = True
+                request.session['user_id'] = user.id
+                request.session['user_name'] = user.username
+                login(request, user)
+                return redirect('index')
+            else:
+                message = '用户不存在或密码错误！'
+                return render(request, 'blog/HTML/login.html', {'message': message, 'login_form': login_form})
+    login_form = UserForm
+    return render(request, 'blog/HTML/login.html', locals())
+
+def logout_view(request):
+    if not request.session.get('is_login', None):
+        return redirect('index')
+    logout(request)
+    return redirect('index')
